f3889bcfcb6ef71b8b2284156d4786362f6d5806
MuhammadTech <muhammatech.uz@outlook.com>
2026-04-17 14:28:03 +0500

    INFRA: Initialize Build Project

--- Sheenam.Api.Test.Unit/DeleteMe.cs
+++ Sheenam.Api.Test.Unit/DeleteMe.cs
@@ -1,4 +1,10 @@
-﻿
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use Comfort and Peace
+//==================================================
+
+using Xunit;
+
 namespace Sheenam.Api.Test.Unit
 {
     public class DeleteMe
