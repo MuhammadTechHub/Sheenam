9b85bc602890e9e3403b768311cd2012790f2e27
MuhammadTech <muhammatech.uz@outlook.com>
2026-06-25 14:14:09 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.Api.Unit.Tests/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+using Xunit;
+namespace Sheenam.Api.Unit.Tests
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
