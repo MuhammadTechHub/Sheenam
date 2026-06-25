817904d40175263c77b1599b2cd41166c27f629a
MuhammadTech <muhammatech.uz@outlook.com>
2026-06-25 15:49:17 +0500

    DATA: Insert `Guest` Model & Migrate

---
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -0,0 +1,15 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests { get; set; }
+    }
+}
