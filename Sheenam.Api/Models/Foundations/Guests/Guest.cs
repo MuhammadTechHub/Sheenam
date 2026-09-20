3d978bb409cdff6ca1940a86a874701041aba352
Muhammadjon <muhammadtech.uz@outlook.com>
2026-09-20 11:14:07 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsInvalidAndLogItAsync -> PASS

--- Sheenam.Api/Models/Foundations/Guests/Guest.cs
+++ Sheenam.Api/Models/Foundations/Guests/Guest.cs
@@ -13,7 +13,6 @@ namespace Sheenam.Api.Models.Foundations.Guests
         public GenderType Gender { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string EmailAddress { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
         public string Address { get; set; }
     }
