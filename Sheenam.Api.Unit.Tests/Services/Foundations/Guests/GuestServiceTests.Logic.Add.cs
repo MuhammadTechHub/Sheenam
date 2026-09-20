185d5413b4c0126b00956250879018b20efcc0eb
Muhammadjon <muhammadtech.uz@outlook.com>
2026-09-20 10:25:29 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsInvalidAndLogItAsync -> FAIL

--- Sheenam.Api.Unit.Tests/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
+++ Sheenam.Api.Unit.Tests/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -7,7 +7,6 @@ using FluentAssertions;
 using Force.DeepCloner;
 using Moq;
 using Sheenam.Api.Models.Foundations.Guests;
-using Xunit;
 
 namespace Sheenam.Api.Unit.Tests.Services.Foundations.Guests
 {
